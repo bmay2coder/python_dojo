0cc961f86d68f35ee09d2d526682534988dd9ca8
Bret May <bret.may@gmail.com>
2024-08-21 18:36:12 -0600

    Added brets class work and updated week two python files

---
+++ brets_class_work.py
@@ -0,0 +1,39 @@
+class ScubaGear:
+    def __init__(self, user, air_tanks, mask, flippers, harpoon, bcd, regulator):
+        self.user = user
+        self.air_tanks = air_tanks
+        self.mask = mask
+        self.flippers = flippers
+        self.harpoon = harpoon
+        self.bcd = bcd
+        self.regulator = regulator
+        self.diver_certified = False
+        self.dives_completed = 0
+
+    def display_info(self):
+        print(self.user)
+        print(self.air_tanks)
+        print(self.mask)
+        print(self.flippers)
+        print(self.harpoon)
+        print(self.bcd)
+        print(self.regulator)
+
+    def dives(self):
+        self.dives_completed += 1
+        print(f"You have completed a dive {self.dives_completed}")
+        return self
+
+    def certification_comlete(self):
+        self.diver_certified = True
+        print(f"You have completed your certification {self.diver_certified}")
+        
+diver_1 = ScubaGear("bret", "Scuba", "Aqua Lung", "flipped", "harpoon", "Oceanic", "Cressi")
+diver_1.display_info()
+diver_1.dives()
+diver_1.dives()
+diver_1.dives()
+diver_1.dives()
+diver_1.certification_comlete
+
+#rint(diver_1.user)
--- week_two.py
+++ week_two.py
@@ -1,14 +1,14 @@
 # Lecture 1
 
-class User:		
-    def __init__(self):
-        self.first_name = "Ada"
-        self.last_name = "Lovelace"
-        self.age = 42
-# Now that you have a class set up with a constructor 
-# You can assign new variables to new users in the outer scope!
-user_ada = User()
-print(user_ada.first_name) # prints Ada
+# class User:		
+#     def __init__(self):
+#         self.first_name = "Ada"
+#         self.last_name = "Lovelace"
+#         self.age = 42
+# # Now that you have a class set up with a constructor 
+# # You can assign new variables to new users in the outer scope!
+# user_ada = User()
+# print(user_ada.first_name) # prints Ada
 
 # Create another user called user_2!
 # Print user_ada's last name.
@@ -19,19 +19,19 @@ print(user_ada.first_name) # prints Ada
 # Sensei Exercise: try just printing the variable, user_ada.
 #   What prints to the terminal?
 
-# class Shoe:
-#     # now our method has 4 parameters (including self)!
-#     def __init__(self, brand, shoe_type, price):
-#     	# we assign them accordingly
-#         self.brand = brand
-#         self.type = shoe_type
-#         self.price = price
-#     	# the status is set to True by default
-#         self.in_stock = True
-# skater_shoe = Shoe("Vans", "Low-top Trainers", 59.99)
-# dress_shoe = Shoe("Jack & Jill Bootery", "Ballet Flats", 29.99)
-# print(skater_shoe.type)	# output: Low-top Trainers
-# print(dress_shoe.type)	# output: Ballet Flats
+class Shoe:
+    # now our method has 4 parameters (including self)!
+    def __init__(self, brand, shoe_type, price):
+    	# we assign them accordingly
+        self.brand = brand
+        self.type = shoe_type
+        self.price = price
+    	# the status is set to True by default
+        self.in_stock = True
+skater_shoe = Shoe("Vans", "Low-top Trainers", 59.99)
+dress_shoe = Shoe("Jack & Jill Bootery", "Ballet Flats", 29.99)
+print(skater_shoe.brand)	# output: Low-top Trainers
+print(dress_shoe.type)	# output: Ballet Flats
 
 # class User:		# here's what we have so far
 #     def __init__(self, name, email):
