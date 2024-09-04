ca6f777f1d7e82b304830180b932eb580b2c8c11
Bret May <bret.may@gmail.com>
2024-09-04 16:17:06 -0600

    Adding star_was and scuba diving app

--- scuba_diving/scuba_diving_website.py
+++ scuba_diving/scuba_diving_website.py
@@ -1,6 +1,5 @@
 from flask import Flask, render_template, request, redirect, session
 app = Flask(__name__)
-# our index route will handle rendering our form
 
 app = Flask(__name__)
 app.secret_key = 'keep it secret, keep it safe' # set a secret key for security purposes
@@ -8,7 +7,13 @@ app.secret_key = 'keep it secret, keep it safe' # set a secret key for security 
 @app.route('/')
 def index():
     return render_template("index.html")
-            
+
+@app.route('/show')
+def show_user():
+    print("Showing the User Info From the Form")
+    print(request.form)
+    return render_template("show.html", name_on_template=session['username'], email_on_template=session['useremail'])
+
 @app.route('/users', methods=['POST'])
 def create_user():
     print("Got Post Info")
@@ -20,21 +25,6 @@ def create_user():
     # Never render a template on a POST request.
     # Instead we will redirect to our index route.
     return redirect('/show')
-
-@app.route('/show')
-def show_user():
-    print("Showing the User Info From the Form")
-    print(request.form)
-    return render_template("show.html", name_on_template=session['username'], email_on_template=session['useremail'])
-
-@app.route('/spear_gun_shots')
-def spear_gun_shots():
-    if "shot" not in session:
-        session["shot"] = 0
-    else:
-        session['shot'] += 1
-    return render_template("spear_gun_shots.html")
-
 
 if __name__ == "__main__":
     app.run(debug=True)
